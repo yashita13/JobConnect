7820855398191b35e79c100d0a78898819a49542
Yashita Bahrani <bahraniyashita@gmail.com>
2025-07-11 03:27:46 +0530

    interview cards aligned

--- components/DisplayTechIcons.tsx
+++ components/DisplayTechIcons.tsx
@@ -10,7 +10,7 @@ const DisplayTechIcons = async({techStack}: TechIconProps) => {
 
     return (
         <div className="flex flex-row">{techIcons.slice(0,3).map(({tech,url},index) => (
-            <div key={tech} className={cn("relative group bg-dark-300 rounded-full p-2 flex-center",index >= 1 && '-ml-3')}>
+            <div key={tech} className={cn("relative group bg-dark-300 rounded-full p-2 flex-center", index >= 1 && '-ml-3')}>
                 <span className="tech-tooltip">{tech}</span>
                 <Image src={url} alt={tech} width={100} height={100} className="size-5" />
 
