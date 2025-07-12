f67a0a8d2ad735a053568bd425faab92f6f33e0f
Yashita Bahrani <bahraniyashita@gmail.com>
2025-07-13 03:02:29 +0530

    generated the UI for interview

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Agent from "@/components/Agent";
 
 const Page = () => {
     return (
-        <div>Interview-Page</div>
+        <>
+            <h3>Interview Generation</h3>
+            <Agent userName="You" userId="user1" type="generate" />
+        </>
     )
 }
 export default Page
